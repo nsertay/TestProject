e71786f53ce51e0256a947dfd1c56c5ec0d07c8e
Nurmukhanbet <nurmukhanbetsertay@gmail.com>
2024-10-07 13:37:49 +0500

    Initial Commit

---
+++ TestProject/TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  TestProject
+//
+//  Created by Nurmukhanbet Sertay on 07.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
